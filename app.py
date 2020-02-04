a4a26378cbadd14ede7baaabc861ee3705c5fe92
laurybenoit <lauryalexandre@me.com>
2020-02-04 17:11:01 -0500

    s

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import Flask
+from flask import Flask, jsonify
+
+# Dependencies and Setup
+import numpy as np
+import datetime as dt
+
+# Python SQL Toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
+
